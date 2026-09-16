575bade142cce2cc5348e923c9fedc6427ffde38
Wolfite <100052562+wlft@users.noreply.github.com>
2026-09-16 13:28:53 +1000

    chore(eslint): remove vue deps from config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,9 +3,6 @@
 const { defineConfig, globalIgnores } = require( 'eslint/config' );
 const { FlatCompat } = require( '@eslint/eslintrc' );
 const js = require( '@eslint/js' );
-const ts_parser = require( '@typescript-eslint/parser' );
-const vue = require( 'eslint-plugin-vue' );
-const vue_parser = require( 'vue-eslint-parser' );
 
 const compat = new FlatCompat( {
 	baseDirectory: __dirname,
@@ -44,20 +41,9 @@ module.exports = defineConfig( [
             'brace-style': 'off',
 			'no-unused-vars': [ 'warn', { args: 'none' } ],
 			'linebreak-style': 'off',
-			'vue/prop-name-casing': 'off',
 			'preserve-caught-error': 'off',
 		},
 	},
-
-	...vue.configs[ 'flat/recommended' ].map( ( cfg ) => (
-		cfg.rules ?
-			{ ...cfg, files: cfg.files || [ '**/*.vue' ] } :
-			cfg
-	) ),
-	...compat.extends( 'wikimedia/vue/wrappers' ).map( ( cfg ) => ( {
-		...cfg,
-		files: [ '**/*.vue' ],
-	} ) ),
 
 	{
 		files: [ 'src/**/*.ts', 'src/**/*.d.ts' ],
@@ -83,40 +69,7 @@ module.exports = defineConfig( [
 	},
 
 	{
-		files: [ 'src/**/*.vue' ],
-		languageOptions: {
-			parser: vue_parser,
-			sourceType: 'module',
-			parserOptions: {
-				parser: ts_parser,
-				extraFileExtensions: [ '.vue' ],
-			},
-		},
-		rules: {
-			camelcase: 'off',
-			'linebreak-style': 'off',
-            'max-statements-per-line': 'off',
-            'brace-style': 'off',
-			'vue/prop-name-casing': 'off',
-			'func-call-spacing': 'off',
-			'es-x/no-optional-chaining': 'off',
-			'@typescript-eslint/explicit-function-return-type': 'off',
-			'@typescript-eslint/explicit-module-boundary-types': 'off',
-			'vue/block-order': [ 'error', {
-				order: [ 'template', 'script', 'style' ],
-			} ],
-			'vue/html-indent': [ 'error', 'tab' ],
-			'vue/html-closing-bracket-newline': 'off',
-			'vue/max-attributes-per-line': [ 'warn', {
-				singleline: 2,
-				multiline: 1,
-			} ],
-			'vue/component-name-in-template-casing': [ 'error', 'PascalCase' ],
-		},
-	},
-
-	{
-		files: [ 'tests/**/*.{js,vue}' ],
+		files: [ 'tests/**/*.{js}' ],
 		languageOptions: {
 			sourceType: 'module',
 		},
